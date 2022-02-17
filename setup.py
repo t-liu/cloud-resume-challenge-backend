8c0029fe3791e98dec9fa18c982e765a5007bd25
t-liu <thomas.s.liu@gmail.com>
2022-02-17 00:16:15 -0500

    add missing comma in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='cloud-resume-challenge-backend',
     version='0.1.0',
     description='Setting up a python package for backend of crc',
-    long_description=open('README.md').read()
+    long_description=open('README.md').read(),
     author='Thomas S. Liu',
     author_email='thomas.s.liu@gmail.com',
     url='https://resume.thomasliu.click',
